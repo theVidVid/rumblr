841c56718f581f4ed023252c504fac6c708da1bc
Ian Vidaurre <ianjvidaurre@gmail.com>
2018-04-27 19:06:05 -0400

    Updated layout, added Log-in, Log-out, and photos.

--- app.rb
+++ app.rb
@@ -16,7 +16,6 @@ end
 
 get '/login' do
     erb :login
-    redirect '/profile'
 end
 
 get '/profile' do
@@ -27,4 +26,40 @@ end
 get '/logout' do
     session.clear
     redirect '/login'
+end
+
+post '/user/login' do 
+    @user = User.find_by(email: params[:email], password: params[:password])
+    if @user != nil
+        session[:id] = @user.id
+        erb :profile
+    else   
+        #Could not find this user. Redirecting them to the signup page
+        redirect '/signup'
+    end 
+end
+
+action="/user/login"
+
+post '/user/post' do 
+    @post = Post.find_by(title: params[:title], content: params[:content])
+    if @post != nil
+        session[:id] = @post.id
+        erb :profile
+    else   
+        #Could not find this user. Redirecting them to the signup page
+        redirect '/profile'
+    end 
+end
+
+action="/user/post"
+
+private 
+#Potentially useful function instead of checking if the user exists
+def user_exists?
+    (session[:id] != nil) ? true : false
+end
+
+def current_user
+    User.find(session[:id])
 end
